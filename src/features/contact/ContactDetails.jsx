57f59b061dd99fc0c5c79116f642f1800fbe29ca
Dusan Madjar <madjardusan@gmail.com>
2024-07-27 10:22:01 +0200

    Add some reorganization Contact files

---
+++ src/features/contact/ContactDetails.jsx
@@ -0,0 +1,69 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Heading from "../../ui/Heading";
+
+const StyledContactDetails = styled.div`
+  padding-inline: 4.8rem;
+  h3 {
+    color: var(--color-grey-800);
+  }
+
+  p {
+    color: var(--color-grey-700);
+    font-size: 1.2rem;
+    letter-spacing: 0.3px;
+    margin-bottom: 1.2rem;
+  }
+
+  .contact__media {
+    display: flex;
+    gap: 3.2rem;
+
+    span {
+      color: var(--color-grey-800);
+      display: block;
+      font-weight: 700;
+      font-size: 1.2rem;
+      text-transform: uppercase;
+      letter-spacing: 0.3px;
+    }
+    a:link,
+    a:visited {
+      color: var(--color-grey-700);
+      font-size: 1.4rem;
+      transition: all 0.1s;
+    }
+    a:hover,
+    a:active {
+      color: var(--color-primary);
+    }
+  }
+`;
+
+function ContactDetails() {
+  return (
+    <StyledContactDetails>
+      <Heading as="h3">Contact Us</Heading>
+      <p>We are open for any suggestion or just to have a chat.</p>
+      <div className="contact__media">
+        <div>
+          <span>Instagram:</span>
+          <Link
+            to="https://www.instagram.com/street_workout_backa_palanka"
+            target="_blank"
+          >
+            street_workout_backa_palanka
+          </Link>
+        </div>
+        <div>
+          <span>Email:</span>
+          <Link to="mailto:swbackapalanka@gmail.com">
+            swbackapalanka@gmail.com
+          </Link>
+        </div>
+      </div>
+    </StyledContactDetails>
+  );
+}
+
+export default ContactDetails;
--- src/features/contact/ContactInfo.jsx
+++ src/features/contact/ContactInfo.jsx
@@ -1,73 +1,17 @@
-import { Link } from "react-router-dom";
 import styled from "styled-components";
-import Heading from "../../ui/Heading";
+import ContactDetails from "./ContactDetails";
 import ContactForm from "./ContactForm";
 
 const StyledContactInfo = styled.div`
   padding: 3.2rem 2.4rem;
   background-color: var(--color-grey-200);
   width: 100%;
-  h3 {
-    color: var(--color-grey-800);
-    margin-left: 4.8rem;
-  }
-
-  p {
-    color: var(--color-grey-700);
-    font-size: 1.2rem;
-    letter-spacing: 0.3px;
-    margin-bottom: 1.2rem;
-    margin-left: 4.8rem;
-  }
-
-  .contact__media {
-    display: flex;
-    gap: 3.2rem;
-    margin-left: 4.8rem;
-
-    span {
-      color: var(--color-grey-800);
-      display: block;
-      font-weight: 700;
-      font-size: 1.2rem;
-      text-transform: uppercase;
-      letter-spacing: 0.3px;
-    }
-    a:link,
-    a:visited {
-      color: var(--color-grey-700);
-      font-size: 1.4rem;
-      transition: all 0.1s;
-    }
-    a:hover,
-    a:active {
-      color: var(--color-primary);
-    }
-  }
 `;
 
 function ContactInfo() {
   return (
     <StyledContactInfo>
-      <Heading as="h3">Contact Us</Heading>
-      <p>We are open for any suggestion or just to have a chat.</p>
-      <div className="contact__media">
-        <div>
-          <span>Instagram:</span>
-          <Link
-            to="https://www.instagram.com/street_workout_backa_palanka"
-            target="_blank"
-          >
-            street_workout_backa_palanka
-          </Link>
-        </div>
-        <div>
-          <span>Email:</span>
-          <Link to="mailto:swbackapalanka@gmail.com">
-            swbackapalanka@gmail.com
-          </Link>
-        </div>
-      </div>
+      <ContactDetails />
       <ContactForm />
     </StyledContactInfo>
   );
