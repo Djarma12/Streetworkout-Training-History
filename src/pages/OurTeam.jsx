6f816ba11dd6ed9ea2d129874932b22445b8ae7a
Dusan Madjar <madjardusan@gmail.com>
2024-07-30 13:12:24 +0200

    Add some refactoring code

--- src/pages/OurTeam.jsx
+++ src/pages/OurTeam.jsx
@@ -6,6 +6,7 @@ import Hero from "../ui/Hero";
 import HeroBackground from "../ui/HeroBackground";
 import HeroContent from "../ui/HeroContent";
 import Section from "../ui/Section";
+import OurTeamCards from "../ui/OurTeamCards";
 
 const StyledOurTeam = styled.div`
   .heading-bar {
@@ -13,61 +14,6 @@ const StyledOurTeam = styled.div`
     align-items: center;
     justify-content: space-between;
     margin-bottom: 4.8rem;
-  }
-
-  .team-members {
-    max-width: 90rem;
-    margin-inline: auto;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 1.6rem;
-
-    li {
-      height: 50rem;
-      overflow: hidden;
-      box-shadow: var(--shadow-md);
-
-      img {
-        height: 100%;
-        width: 100%;
-        object-fit: cover;
-        object-position: top;
-        border-radius: var(--border-radius-sm);
-
-        &:hover + div {
-          transform: translateY(-19rem);
-        }
-      }
-    }
-
-    .team-member-info {
-      background-color: var(--color-grey-800);
-      color: var(--color-red-50);
-      text-align: center;
-      padding-block: 9.6rem 6.4rem;
-      padding-inline: 1.2rem;
-      clip-path: polygon(100% 0, 100% 38%, 100% 100%, 0 100%, 0% 38%);
-      transition: all 0.1s ease-in-out;
-      font-weight: 500;
-      font-size: 2rem;
-      letter-spacing: 0.6px;
-      position: relative;
-
-      &::before {
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        height: 19rem;
-        clip-path: polygon(100% 0, 100% 3%, 0 41%, 0% 0%);
-        background-color: var(--color-blue-900);
-      }
-
-      &:hover {
-        transform: translateY(-19rem);
-      }
-    }
   }
 `;
 
@@ -89,47 +35,7 @@ function OurTeam() {
             <Button variation="primary">Appointment</Button>
           </Link>
         </div>
-        <ul className="team-members">
-          <li>
-            <img
-              src="public/team-members/team-member-1.jpg"
-              alt="Team member"
-            />
-            <div className="team-member-info">
-              <span>Cedo Milovanovic</span>
-            </div>
-          </li>
-          <li>
-            <img
-              src="public/team-members/team-member-2.jpg"
-              alt="Team member"
-            />
-          </li>
-          <li>
-            <img
-              src="public/team-members/team-member-3.jpg"
-              alt="Team member"
-            />
-          </li>
-          <li>
-            <img
-              src="public/team-members/team-member-4.jpg"
-              alt="Team member"
-            />
-          </li>
-          <li>
-            <img
-              src="public/team-members/team-member-5.jpg"
-              alt="Team member"
-            />
-          </li>
-          <li>
-            <img
-              src="public/team-members/team-member-6.jpg"
-              alt="Team member"
-            />
-          </li>
-        </ul>
+        <OurTeamCards />
       </Section>
     </StyledOurTeam>
   );
---
+++ src/ui/OurTeamCards.jsx
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+import { teamMembers } from "../assets/team-members.json";
+
+const StyledOurTeamCards = styled.ul`
+  max-width: 90rem;
+  margin-inline: auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1.6rem;
+
+  li {
+    height: 50rem;
+    overflow: hidden;
+    box-shadow: var(--shadow-md);
+
+    img {
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+      object-position: top;
+      border-radius: var(--border-radius-sm);
+
+      &:hover + div {
+        transform: translateY(-19rem);
+      }
+    }
+  }
+
+  .team-member-info {
+    background-color: var(--color-grey-800);
+    color: var(--color-red-50);
+    text-align: center;
+    padding-block: 9.6rem 6.4rem;
+    padding-inline: 1.2rem;
+    clip-path: polygon(100% 0, 100% 38%, 100% 100%, 0 100%, 0% 38%);
+    transition: all 0.1s ease-in-out;
+    font-weight: 500;
+    font-size: 2rem;
+    letter-spacing: 0.6px;
+    position: relative;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      height: 19rem;
+      clip-path: polygon(100% 0, 100% 3%, 0 41%, 0% 0%);
+      background-color: var(--color-blue-900);
+    }
+
+    &:hover {
+      transform: translateY(-19rem);
+    }
+  }
+`;
+
+function OurTeamCards() {
+  return (
+    <StyledOurTeamCards>
+      {teamMembers.map((teamMember) => (
+        <li key={teamMember.id}>
+          <img src={teamMember.imgpath} alt="Team member" />
+          <div className="team-member-info">
+            <span>{teamMember.name}</span>
+          </div>
+        </li>
+      ))}
+    </StyledOurTeamCards>
+  );
+}
+
+export default OurTeamCards;
