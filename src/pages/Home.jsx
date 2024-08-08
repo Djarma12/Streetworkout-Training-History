6dba10abc5b4377ca8cbd75fabd71713c0274ac8
Dusan Madjar <madjardusan@gmail.com>
2024-08-08 16:55:44 +0200

    Add video from the humanitarian rally

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -54,6 +54,15 @@ function Home() {
           <ChoseUsReasons />
         </div>
       </Section>
+      <Section>
+        <iframe
+          width="100%"
+          height="600px"
+          src="https://www.youtube.com/embed/7IhGzGfQg9E?si=IaHtb8330hXMplWa"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen></iframe>
+      </Section>
     </StyledHome>
   );
 }
