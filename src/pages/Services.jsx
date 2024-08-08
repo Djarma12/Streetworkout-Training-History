1b2975d74145144459681d9db5915af6279024c6
Dusan Madjar <madjardusan@gmail.com>
2024-08-09 00:37:25 +0200

    Add style for ProductCard text and json file for all products data

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,66 +1,20 @@
 import styled from "styled-components";
-import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import Hero from "../ui/Hero";
 import HeroBackground from "../ui/HeroBackground";
 import HeroContent from "../ui/HeroContent";
 import Section from "../ui/Section";
+import { ourProducts } from "../assets/our-products.json";
+import ProductCard from "../ui/ProductCard";
 
 const StyledServices = styled.div`
-  text-align: center;
+  h5 {
+    text-align: center;
+  }
   .shop {
     display: flex;
+    /* flex-wrap: wrap; */
     gap: 6.4rem;
-  }
-  .card {
-    perspective: 150rem;
-    -moz-perspective: 150rem;
-    position: relative;
-    height: 52rem;
-    width: 100%;
-
-    &__side {
-      height: 52rem;
-      transition: all 0.8s ease;
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      -webkit-backface-visibility: hidden;
-      backface-visibility: hidden;
-      border-radius: 3px;
-      overflow: hidden;
-      box-shadow: var(--shadow-lg);
-      img {
-        height: 37rem;
-        width: 100%;
-        object-fit: cover;
-      }
-
-      &--front {
-        /* background-color: var(--color-grey-50); */
-        background-color: orange;
-      }
-
-      &--back {
-        transform: rotateY(180deg);
-        background-color: blue;
-      }
-    }
-    &:hover {
-      .card__side--front {
-        background-color: blue;
-        transform: rotateY(-180deg);
-      }
-
-      .card__side--back {
-        transform: rotateY(0deg);
-      }
-    }
-
-    &__details {
-      padding: 2.4rem;
-    }
   }
 `;
 
@@ -77,56 +31,11 @@ function Services() {
           <span className="secondary">Our Clothes</span>Choose your best fit{" "}
           <span>Clothes</span>
         </Heading>
-        <div className="shop">
-          <div className="card">
-            <div className="card__side card__side--front">
-              <img src="public/shop-products/t-shirt-1.jpg" alt="" />
-              <div className="card__details">
-                <p>
-                  <span>&#8364;12</span>
-                </p>
-              </div>
-            </div>
-            <div className="card__side card__side--back">
-              <img src="public/shop-products/t-shirt-2.jpg" alt="" />
-              <div className="card__details">
-                <Button>Buy Now!</Button>
-              </div>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card__side card__side--front">
-              <img src="public/shop-products/sweatshirt-1.jpg" alt="" />
-              <div className="card__details">
-                <p>
-                  <span>&#8364;12</span>
-                </p>
-              </div>
-            </div>
-            <div className="card__side card__side--back">
-              <img src="public/shop-products/sweatshirt-2.jpg" alt="" />
-              <div className="card__details">
-                <Button>Buy Now!</Button>
-              </div>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card__side card__side--front">
-              <img src="public/shop-products/t-shirt-3.jpg" alt="" />
-              <div className="card__details">
-                <p>
-                  <span>&#8364;12</span>
-                </p>
-              </div>
-            </div>
-            <div className="card__side card__side--back">
-              <img src="public/shop-products/t-shirt-4.jpg" alt="" />
-              <div className="card__details">
-                <Button>Buy Now!</Button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <ul className="shop">
+          {ourProducts.map((product) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
+        </ul>
       </Section>
     </StyledServices>
   );
---
+++ src/ui/ProductCard.jsx
@@ -0,0 +1,143 @@
+import styled from "styled-components";
+import Button from "./Button";
+import { Link } from "react-router-dom";
+
+const StyledProductCard = styled.li`
+  height: 52rem;
+  width: 100%;
+  .card {
+    height: 100%;
+    perspective: 150rem;
+    -moz-perspective: 150rem;
+    position: relative;
+    &__side {
+      height: 52rem;
+      transition: all 0.8s ease;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      -webkit-backface-visibility: hidden;
+      backface-visibility: hidden;
+      border-radius: 3px;
+      overflow: hidden;
+      box-shadow: var(--shadow-lg);
+
+      &--front {
+        background-color: var(--color-grey-0);
+      }
+
+      &--back {
+        transform: rotateY(180deg);
+        background-color: var(--color-red-50);
+      }
+    }
+    &:hover {
+      .card__side--front {
+        transform: rotateY(-180deg);
+      }
+
+      .card__side--back {
+        transform: rotateY(0deg);
+      }
+    }
+
+    &__image-holder {
+      box-shadow: var(--shadow-md);
+      height: 37rem;
+    }
+
+    &__image {
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+    }
+
+    &__details {
+      height: 15rem;
+      padding: 2.4rem;
+      border-top: 1px solid var(--color-grey-50);
+      &--front {
+        text-align: center;
+      }
+      &--back {
+        display: flex;
+        align-items: center;
+        gap: 1.2rem;
+      }
+    }
+
+    &__name {
+      font-size: 1.8rem;
+      font-weight: 700;
+      letter-spacing: 0.8px;
+      color: var(--color-grey-700);
+    }
+
+    &__type {
+      font-size: 1.2rem;
+      font-weight: 700;
+      letter-spacing: 0.8px;
+      color: var(--color-grey-600);
+      word-wrap: break-word;
+      hyphens: auto;
+      text-align: justify;
+    }
+
+    &__value {
+      &--currency {
+        font-size: 3rem;
+        font-weight: 700;
+        margin-right: 0.8rem;
+      }
+      &--price {
+        color: var(--color-primary-dark);
+        font-size: 5.2rem;
+        font-weight: 900;
+      }
+    }
+  }
+`;
+
+function ProductCard({ product }) {
+  const { name, type, frontImg, backImg, price } = product;
+  return (
+    <StyledProductCard>
+      <div className="card">
+        <div className="card__side card__side--front">
+          <div className="card__image-holder">
+            <img
+              src={frontImg}
+              className="card__image"
+              alt="Front product image"
+            />
+          </div>
+          <div className="card__details card__details--front">
+            <span className="card__name">{name}</span>
+            <p className="card__value">
+              <sub className="card__value--currency">&#8364;</sub>
+              <span className="card__value--price">{price}</span>
+            </p>
+          </div>
+        </div>
+        <div className="card__side card__side--back">
+          <div className="card__image-holder">
+            <img
+              src={backImg}
+              className="card__image"
+              alt="Back product image"
+            />
+          </div>
+          <div className="card__details card__details--back">
+            <span className="card__type">{type}</span>
+            <Link to="/contact">
+              <Button>Buy!</Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </StyledProductCard>
+  );
+}
+
+export default ProductCard;
